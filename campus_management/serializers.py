80d1ac1c48468dbd6e6395c94763e28e64151565
lolli-17 <lollomannoni@hotmail.com>
2025-09-12 13:59:22 +0200

    fatto

--- campus_management/serializers.py
+++ campus_management/serializers.py
@@ -158,7 +158,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CustomUser
 		fields = (
-			'id', 'username', 'email', 'role', 'isFirstAccess', 'first_name', 'last_name', 'room',
+			'id', 'username', 'email', 'role', 'isFirstAccess', 'first_name', 'last_name',
 			'is_staff', 'is_active', 'date_joined', 'last_login', 'groups', 'user_permissions', 'password'
 		)
 		read_only_fields = ('date_joined', 'last_login',)

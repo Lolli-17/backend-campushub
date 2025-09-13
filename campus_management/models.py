628e9cf14cfb2318efe0e0f05282f1098cbc24f8
lolli-17 <lollomannoni@hotmail.com>
2025-09-13 16:08:19 +0200

    user room

--- campus_management/models.py
+++ campus_management/models.py
@@ -30,7 +30,7 @@ class CustomUser(AbstractUser, BaseModel):
 	phoneNumber = models.BigIntegerField(null=True)
 	campus = models.OneToOneField(Campus, on_delete=models.DO_NOTHING, null=True)
 	balance = models.FloatField(default=0.0)
-	room = models.ForeignKey(Apartment, on_delete=models.DO_NOTHING, null=True)
+	apartment = models.ForeignKey(Apartment, on_delete=models.DO_NOTHING, null=True)
 
 	def __str__(self):
 		return self.username

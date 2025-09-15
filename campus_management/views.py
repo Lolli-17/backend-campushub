04781fa5711f6dadd3fbf3a5b6a9a240716c702f
lolli-17 <lollomannoni@hotmail.com>
2025-09-15 13:29:53 +0200

    import

--- campus_management/views.py
+++ campus_management/views.py
@@ -1,7 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny, DjangoModelPermissions
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework.views import APIView
 from django.utils import timezone
 from django.contrib.auth.models import Group
 from .models import (
